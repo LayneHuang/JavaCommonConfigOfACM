41448e5be12afb1184ebae5ffeb974509301e4cb
laynehuang <hl_mark@163.com>
2019-03-19 20:57:28 +0800

    cf f

--- Main.java
+++ Main.java
@@ -4,7 +4,74 @@ import java.util.*;
 public class Main {
 
     private static void solve() {
+        int n = in.nextInt();
+        String s = in.next();
+        String ns = "";
 
+        for (int i = 0; i < n; ++i) {
+            if (i == 0 || (s.charAt(i) != s.charAt(i - 1))) {
+                ns += s.charAt(i);
+            }
+        }
+
+        int[][][] dp = new int[n + 10][n + 10][30];
+        n = ns.length();
+
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                for (int k = 0; k < 26; ++k) {
+                    dp[i][j][k] = n;
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            dp[i][i][ns.charAt(i) - 'a'] = 1;
+        }
+
+        for (int len = 2; len <= n; ++len) {
+            for (int beg = 0; beg + len - 1 < n; ++beg) {
+                int ed = beg + len - 1;
+                int mx = dp[beg][ed][0];
+                for (int k = 0; k < 26; ++k) {
+                    mx = Math.min(mx, dp[beg][ed][k]);
+                }
+                int c = 0;
+                int d = 0;
+                if (beg > 0) {
+                    c = ns.charAt(beg - 1) - 'a';
+                }
+                if (ed + 1 < n) {
+                    d = ns.charAt(ed + 1) - 'a';
+                }
+                if (beg > 0) {
+                    dp[beg - 1][ed][c] = Math.min(dp[beg - 1][ed][c], mx + 1);
+                }
+                if (ed < n - 1) {
+                    dp[beg][ed + 1][d] = Math.min(dp[beg][ed + 1][d], mx + 1);
+                }
+                if (beg > 0 && ed < n - 1 && c == d) {
+                    dp[beg - 1][ed + 1][c] = Math.min(dp[beg - 1][ed + 1][c], mx + 1);
+                }
+                for (int k = 0; k < 26; ++k) {
+                    if (beg > 0) {
+                        dp[beg - 1][ed][k] = Math.min(dp[beg - 1][ed][k], dp[beg][ed][k]);
+                    }
+                    if (ed < n - 1) {
+                        dp[beg][ed + 1][k] = Math.min(dp[beg][ed + 1][k], dp[beg][ed][k]);
+                    }
+                    if (beg > 0 && ed < n - 1 && c == d) {
+                        dp[beg - 1][ed + 1][k] = Math.min(dp[beg - 1][ed + 1][k], dp[beg][ed][k]);
+                    }
+                }
+            }
+        }
+
+        out.println(dp[0][1][0]);
+        int ans = n;
+        for (int k = 0; k < 26; ++k) {
+            ans = Math.min(ans, dp[0][n - 1][k]);
+        }
+        out.println(ans);
     }
 
     // -----------------------  Common Func ------------------------
